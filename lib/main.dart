fe500a08978151a4ab64be298751cf9c72d22ffe
Khemraj <khemraj@tutopiya.comgit>
2025-07-19 13:06:25 +0530

    Dice app development code for flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Dicee());
+}
+class Dicee extends StatelessWidget {
+  const Dicee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
+
